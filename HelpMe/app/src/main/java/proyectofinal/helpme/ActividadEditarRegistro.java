29b2c82913f6cfc697451df4d4b04d54493f4b3c
Ilanit Jamilis <ilanitsj@gmail.com>
2017-06-16 12:03:59 -0300

    actualizo todo ok

--- HelpMe/app/src/main/java/proyectofinal/helpme/ActividadEditarRegistro.java
+++ HelpMe/app/src/main/java/proyectofinal/helpme/ActividadEditarRegistro.java
@@ -1,11 +1,15 @@
 package proyectofinal.helpme;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ActividadEditarRegistro extends Fragment implements View.OnClickListener {
 
@@ -18,12 +22,36 @@ public class ActividadEditarRegistro extends Fragment implements View.OnClickLis
         // Inflate the layout for this fragment
 
         View vistaADevolver = inflater.inflate(R.layout.actividad_editar_registro, container, false);
+
+        Button aceptarEditarRegistro = (Button)vistaADevolver.findViewById(R.id.btnAceptarEditarMisDatos);
+        aceptarEditarRegistro.setOnClickListener(this);
+
         return vistaADevolver;
     }
 
     public void onClick(View vista) {
         ActividadNavigationDrawer ActividadNavigationDrawer;
         ActividadNavigationDrawer = (ActividadNavigationDrawer) getActivity();
+
+        AlertDialog.Builder adb = new AlertDialog.Builder(getActivity());
+
+        adb.setTitle("¿Está seguro que quiere editar sus datos?");
+        //Deberian ir los datos para que los vea
+
+        adb.setIcon(android.R.drawable.ic_dialog_alert);
+
+        adb.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                //Editar mis datos
+                dialog.cancel();
+            } });
+
+
+        adb.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            } });
+        adb.show();
     }
 
 }
